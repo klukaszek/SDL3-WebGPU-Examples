c85e1b36adc816e7d3d69137a8629009a4bb3a58
klukaszek <klukaszek@users.noreply.github.com>
2025-01-09 23:15:42 +0000

    deploy: 0a47d9dcd5c75fb829fdd6ebdff89a94ac6966ef

--- SDL_gpu_examples.js
+++ SDL_gpu_examples.js
@@ -35,7 +35,7 @@ if (ENVIRONMENT_IS_NODE) {}
 
 // --pre-jses are emitted after the Module integration code, so that they can
 // refer to Module (if they choose; they can also define Module)
-// include: /tmp/tmprgi751sl.js
+// include: /tmp/tmpd6vkqymg.js
 if (!Module.expectedDataFileDownloads) {
   Module.expectedDataFileDownloads = 0;
 }
@@ -516,23 +516,23 @@ Module.expectedDataFileDownloads++;
   });
 })();
 
-// end include: /tmp/tmprgi751sl.js
-// include: /tmp/tmpps8f98qk.js
+// end include: /tmp/tmpd6vkqymg.js
+// include: /tmp/tmpfu6aewj6.js
 // All the pre-js content up to here must remain later on, we need to run
 // it.
 if (Module["$ww"] || (typeof ENVIRONMENT_IS_PTHREAD != "undefined" && ENVIRONMENT_IS_PTHREAD)) Module["preRun"] = [];
 
 var necessaryPreJSTasks = Module["preRun"].slice();
 
-// end include: /tmp/tmpps8f98qk.js
-// include: /tmp/tmphgyuq3e2.js
+// end include: /tmp/tmpfu6aewj6.js
+// include: /tmp/tmpxw14udxg.js
 if (!Module["preRun"]) throw "Module.preRun should exist because file support used it; did a pre-js delete it?";
 
 necessaryPreJSTasks.forEach(task => {
   if (Module["preRun"].indexOf(task) < 0) throw "All preRun tasks that exist before user pre-js code should remain after; did you replace Module or modify Module.preRun?";
 });
 
-// end include: /tmp/tmphgyuq3e2.js
+// end include: /tmp/tmpxw14udxg.js
 // Sometimes an existing Module object exists with properties
 // meant to overwrite the default module functionality. Here
 // we collect those properties and reapply _after_ we configure
